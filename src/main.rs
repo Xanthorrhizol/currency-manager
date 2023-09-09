9c7d764477a85fcf2882aba86fca5f8f2cc7ba58
Xanthorrhizol <xanthorrhizol@proton.me>
2023-09-09 10:39:46 +0900

    F: automatically update whole date passed in once

--- src/main.rs
+++ src/main.rs
@@ -31,57 +31,76 @@ async fn main() {
 }
 
 async fn update(path: PathBuf, date: Option<String>) {
-    let date = match date {
-        Some(d) => d,
-        None => Utc::now().naive_local().date().to_string(),
+    let dates = match date {
+        Some(d) => vec![d],
+        None => {
+            let mut reader = Reader::from_path(path.clone()).unwrap();
+            let mut date = Utc::now().naive_local().date();
+            let mut dates = Vec::new();
+            match reader.records().last() {
+                Some(Ok(record)) => {
+                    let start_date =
+                        chrono::NaiveDate::parse_from_str(&record[0], "%Y-%m-%d").unwrap();
+                    while date > start_date {
+                        dates.push(date.to_string());
+                        date -= chrono::Duration::days(1);
+                    }
+                }
+                _ => dates.push(date.to_string()),
+            };
+            dates.reverse();
+            dates
+        }
     };
-    let body = format!("{{\"dataBody\":{{\"ricInptRootInfo\":{{\"serviceType\":\"GU\",\"serviceCode\":\"F3733\",\"nextServiceCode\":\"\",\"pkcs7Data\":\"\",\"signCode\":\"\",\"signData\":\"\",\"useSign\":\"\",\"useCert\":\"\",\"permitMultiTransaction\":\"\",\"keepTransactionSession\":\"\",\"skipErrorMsg\":\"\",\"mode\":\"\",\"language\":\"ko\",\"exe2e\":\"\",\"hideProcess\":\"\",\"clearTarget\":\"\",\"callBack\":\"shbObj.fncF3733Callback\",\"exceptionCallback\":\"\",\"requestMessage\":\"\",\"responseMessage\":\"\",\"serviceOption\":\"\",\"pcLog\":\"\",\"preInqForMulti\":\"\",\"makesum\":\"\",\"removeIndex\":\"\",\"redirectUrl\":\"\",\"preInqKey\":\"\",\"_multi_transfer_\":\"\",\"_multi_transfer_count_\":\"\",\"_multi_transfer_amt_\":\"\",\"userCallback\":\"\",\"menuCode\":\"\",\"certtype\":\"\",\"fromMulti\":\"\",\"fromMultiIdx\":\"\",\"isRule\":\"N\",\"webUri\":\"/index.jsp\",\"gubun\":\"\",\"tmpField2\":\"\"}},\"조회구분\":\"\",\"조회일자\":\"{}{}{}\",\"고시회차\":\"\"}},\"dataHeader\":{{\"trxCd\":\"RSRFO0100A01\",\"language\":\"ko\",\"subChannel\":\"49\",\"channelGbn\":\"D0\"}}}}", &date[0..4], &date[5..7], &date[8..10]);
-    let client = reqwest::Client::new();
-    let res = if let JsonValue::Object(o) = json::parse(
-        &client
-            .post("https://bank.shinhan.com/serviceEndpoint/httpDigital")
-            .body(body)
-            .header("Content-Type", "application/json")
-            .send()
-            .await
-            .unwrap()
-            .text()
-            .await
-            .unwrap(),
-    )
-    .unwrap()
-    {
-        let mut map = HashMap::new();
-        if let Some(JsonValue::Object(o)) = o.get("dataBody") {
-            if let Some(JsonValue::Array(array)) = o.get("R_RIBF3733_1") {
-                for elem in array.iter() {
-                    if let JsonValue::Object(o) = elem {
-                        let currency_type =
-                            o.get("통화CODE").unwrap().as_str().unwrap().to_string();
-                        let to_krw = o.get("지폐매입환율").unwrap().as_f32().unwrap();
-                        map.insert(currency_type, to_krw);
+    let mut file = File::options().append(true).open(path).unwrap();
+    for date in dates.iter() {
+        let body = format!("{{\"dataBody\":{{\"ricInptRootInfo\":{{\"serviceType\":\"GU\",\"serviceCode\":\"F3733\",\"nextServiceCode\":\"\",\"pkcs7Data\":\"\",\"signCode\":\"\",\"signData\":\"\",\"useSign\":\"\",\"useCert\":\"\",\"permitMultiTransaction\":\"\",\"keepTransactionSession\":\"\",\"skipErrorMsg\":\"\",\"mode\":\"\",\"language\":\"ko\",\"exe2e\":\"\",\"hideProcess\":\"\",\"clearTarget\":\"\",\"callBack\":\"shbObj.fncF3733Callback\",\"exceptionCallback\":\"\",\"requestMessage\":\"\",\"responseMessage\":\"\",\"serviceOption\":\"\",\"pcLog\":\"\",\"preInqForMulti\":\"\",\"makesum\":\"\",\"removeIndex\":\"\",\"redirectUrl\":\"\",\"preInqKey\":\"\",\"_multi_transfer_\":\"\",\"_multi_transfer_count_\":\"\",\"_multi_transfer_amt_\":\"\",\"userCallback\":\"\",\"menuCode\":\"\",\"certtype\":\"\",\"fromMulti\":\"\",\"fromMultiIdx\":\"\",\"isRule\":\"N\",\"webUri\":\"/index.jsp\",\"gubun\":\"\",\"tmpField2\":\"\"}},\"조회구분\":\"\",\"조회일자\":\"{}{}{}\",\"고시회차\":\"\"}},\"dataHeader\":{{\"trxCd\":\"RSRFO0100A01\",\"language\":\"ko\",\"subChannel\":\"49\",\"channelGbn\":\"D0\"}}}}", &date[0..4], &date[5..7], &date[8..10]);
+        let client = reqwest::Client::new();
+        let res = if let JsonValue::Object(o) = json::parse(
+            &client
+                .post("https://bank.shinhan.com/serviceEndpoint/httpDigital")
+                .body(body)
+                .header("Content-Type", "application/json")
+                .send()
+                .await
+                .unwrap()
+                .text()
+                .await
+                .unwrap(),
+        )
+        .unwrap()
+        {
+            let mut map = HashMap::new();
+            if let Some(JsonValue::Object(o)) = o.get("dataBody") {
+                if let Some(JsonValue::Array(array)) = o.get("R_RIBF3733_1") {
+                    for elem in array.iter() {
+                        if let JsonValue::Object(o) = elem {
+                            let currency_type =
+                                o.get("통화CODE").unwrap().as_str().unwrap().to_string();
+                            let to_krw = o.get("지폐매입환율").unwrap().as_f32().unwrap();
+                            map.insert(currency_type, to_krw);
+                        }
                     }
                 }
             }
-        }
-        map
-    } else {
-        panic!("couldn't get data from shinhan bank");
-    };
+            map
+        } else {
+            panic!("couldn't get data from shinhan bank");
+        };
 
-    let mut file = File::options().append(true).open(path).unwrap();
-    let krw_diff = 0;
-    let jpy_diff = 0;
-    let usd_diff = 0;
-    let jpy_to_krw = res.get("JPY").unwrap();
-    let usd_to_krw = res.get("USD").unwrap();
-    let _ = file.write(
-        format!(
-            "{},{},{},{},{},{}\n",
-            date, krw_diff, jpy_diff, usd_diff, jpy_to_krw, usd_to_krw,
-        )
-        .as_bytes(),
-    );
+        let krw_diff = 0;
+        let jpy_diff = 0;
+        let usd_diff = 0;
+        let jpy_to_krw = res.get("JPY").unwrap();
+        let usd_to_krw = res.get("USD").unwrap();
+        let _ = file.write(
+            format!(
+                "{},{},{},{},{},{}\n",
+                date, krw_diff, jpy_diff, usd_diff, jpy_to_krw, usd_to_krw,
+            )
+            .as_bytes(),
+        );
+    }
 }
 
 fn report(path: PathBuf) {
